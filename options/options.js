871df10ed45084947eaa190cc0f39f0692b27a09
Crowdbotics <engineering@crowdbotics.com>
2023-01-24 14:28:19 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "twist-ty-dev-59167",
+  url: "https://twist-ty-dev-59167.botics.co",
+  api: "https://twist-ty-dev-59167.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
